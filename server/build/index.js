7eff8ebe279ceb71a5224f8fdf564f1542e02cbf
DIO0550 <diot12345@yahoo.co.jp>
2025-04-06 17:01:38 +0900

    お試し実装

---
+++ server/build/index.js
@@ -0,0 +1,23 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+const server = new McpServer({
+    name: "mcp-example",
+    version: "1.0.0",
+});
+server.tool("double_number", "与えられた数値を2倍にする", { num: z.number().describe("数値") }, ({ num }) => ({ content: [{ type: "text", text: (num * 2).toString() }] }));
+server.tool("uuid", "UUIDを生成する", {}, () => {
+    const uuid = crypto.randomUUID();
+    return { content: [{ type: "text", text: uuid }] };
+});
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Example MCP Server running on stdio");
+}
+main().catch((error) => {
+    console.error("Fatal error in main():", error);
+    process.exit(1);
+});
+const transport = new StdioServerTransport();
+await server.connect(transport);
